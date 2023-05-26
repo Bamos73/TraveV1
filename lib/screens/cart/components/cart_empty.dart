858a2a758b276b6ce77d2f1f34f76249f96cf6ee
Bamos73 <samiouattara73@gmail.com>
2023-05-26 22:48:09 +0000

    final Commit

--- lib/screens/cart/components/cart_empty.dart
+++ lib/screens/cart/components/cart_empty.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:shopapp/components/default_button.dart';
 import 'package:shopapp/components/main_screens.dart';
-import 'package:shopapp/constants.dart';
 import 'package:shopapp/size_config.dart';
 
 class EmptyCart extends StatelessWidget {
@@ -15,9 +15,9 @@ class EmptyCart extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Icons.shopping_cart_checkout,
-            size: getProportionateScreenWidth(80),
-            color: kSecondaryColor.withOpacity(0.6),),
+          Center(
+            child: Lottie.asset("assets/lottiefiles/92272-add-to-cart.json"),
+          ),
           Text("TON PANIER EST VIDE. DÉCOUVRE LES NOUVEAUTÉS.",
             style: TextStyle(
                 color:Colors.black.withOpacity(0.8),
